ff0ad82b0c01b35a4fddcd9944387c760f7e5e85
Lakanen Antti-Jussi <antti-jussi.lakanen@jyu.fi>
2024-01-10 12:51:37 +0200

    Luento 1

---
+++ Luento01/HelloWorld/HelloWorld.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// @author Antti-Jussi Lakanen
+/// @version 09.01.2024
+/// <summary>
+/// Ensimmäinen tietokoneohjelmani. Tulostaa tekstin "Moikka maailma". 
+/// </summary>
+public class HelloWorld
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void Main()
+    {
+        System.Console.WriteLine("Moikka maailma");
+        System.Console.WriteLine("Terveisiä Agoran auditoriosta");
+    }
+}
