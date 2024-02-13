67ab8dcda5e8603b903386772125a695092a60cb
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2024-02-13 10:08:03 +0200

    Luento 11

---
+++ Luento11/ElaimetPerinnalla/Elaimet.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Elaimet;
+
+/// @author Antti-Jussi Lakanen
+/// @version 2024
+/// 
+/// <summary>
+/// Eläimet perinnällä.
+/// Toimikoon tämä johdantona olio-ohjelmointiin.
+/// Tämä on "pääluokka", josta ohjelma käynnistetään ja toimii
+/// ikään kuin käyttöliittymänä.
+/// </summary>
+public class Elaimet
+{
+    /// <summary>
+    /// Pääohjelmassa luodaan eläinoliot ja muutetaan 
+    /// niiden arvoja ja kutsutaan metodeja.
+    /// </summary>
+    public static void Main()
+    {
+    }
+}
---
+++ Luento11/SilmukatJaTaulukot/SilmukatJaTaulukot.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// @author Antti-Jussi Lakanen
+/// @version 13.02.2024
+/// 
+/// <summary>
+/// Luento 11.
+/// </summary>
+public class SilmukatJaTaulukot
+{
+    /// <summary>
+    /// Luento 11.
+    /// </summary>
+    public static void Main()
+    {
+        // TODO: Tehdään 10 pituinen taulukko johon luvut 1..10
+        // TODO: Tulosta taulukko
+        // TODO: Tehdään 7 pituinen taulukko johon luvut 7..1
+        // TODO: Taulukko funktion paluuarvona
+    }
+
+}
