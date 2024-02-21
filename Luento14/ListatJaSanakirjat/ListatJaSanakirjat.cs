d1531ce8fe5e710fa3ae561326a35b3739e02a89
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2024-02-21 10:05:54 +0200

    Luento 14 pohja

---
+++ Luento14/ListatJaSanakirjat/ListatJaSanakirjat.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// <summary>
+/// Luento 14. Listat ja sanakirjat. 
+/// </summary>
+public class ListatJaSanakirjat
+{
+    public static void Main()
+    {
+        #region Taulukon pituutta ei voi ylittää
+
+        //   
+
+        #endregion
+
+        #region String-lista
+
+        // 
+
+        #endregion
+
+        #region Listan Remove-metodi
+
+        // Remove poistaa JA palauttaa tiedon poistettiinko jotakin
+
+        #endregion
+
+        #region Luennolle saapuvien nimet
+
+        // 
+
+        #endregion
+
+        #region Henkilötunnukset
+
+        //
+
+        #endregion
+
+        #region Sisältääkö sanakirja avaimen
+
+        // 
+
+        #endregion
+
+        #region Avain-arvoparien käsittely
+
+        // 
+
+        #endregion
+
+        #region Merkkien määrä -esimerkki
+
+        // 
+
+        #endregion
+
+        #region Henkilötunnusten syöttö -esimerkki
+
+        //
+
+        #endregion
+    }
+}
