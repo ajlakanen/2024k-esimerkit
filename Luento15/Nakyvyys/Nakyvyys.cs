bb24e9cc0f1c879b0d132442aed56c26ccfde980
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2024-02-27 11:25:32 +0200

    Luento 15

--- Luento15/Nakyvyys/Nakyvyys.cs
+++ Luento15/Nakyvyys/Nakyvyys.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Collections.Generic;
 
 /// <summary>
-/// Luento 15:
+/// Luento 15
 ///  - Paikallinen muuttuja
 ///  - Muuttujien näkyvyys
 ///  - Lohko
@@ -13,8 +13,98 @@ using System.Collections.Generic;
 /// </summary>
 public class Nakyvyys
 {
+    // vain harvinaisia erityistilanteita varten
+    public static int munMuuttuja = 3;
+
     public static void Main()
     {
+        int luku = 9; // Näkyy vain pääohjelmassa
+        munMuuttuja++;
+        Console.WriteLine(munMuuttuja);
+        A();
+        Console.WriteLine(munMuuttuja);
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine($"Moikka whilesta: {i}");
+                i++;
+            }
+        }
+
+        int[] luvut = { 9, 7, 11 };
         
+        // Oletetaan niin että haluamme käsitellä tätä tietoa
+        // aliohjelmassa/funktiossa.
+
+        // Tietoja voidaan käsitellä C#:ssa ainakin seuraavilla
+        // tavoilla: 
+        // 1. Muutetaan parametrina saatua asiaa, josta seuraa
+        // sivuvaikutuksia
+        // Tästä esimerkkinä on taulukon sisällön muuttaminen
+        // aliohjelmassa
+        MuutaPaikkoja(luvut, 2, 1);
+        
+        // 2. Ei muuteta parametrina saatua tietoa, vaan
+        // luodaan uusi tieto ja palautetaan se. 
+        // (Tätä voidaan kutsua "funktionaaliseksi" lähestymistavaksi.)
+        List<int> luvutLista = new List<int>() { 9, 7, 11 };
+        List<int> uusiLista = MuutaPaikkoja(luvutLista, 2, 1);
+
+        int luku3 = 3;
+        Console.WriteLine(luku3);
+        MuutaLukua(luku3);
+        Console.WriteLine(luku3);
+    }
+
+    public static void MuutaLukua(int munLuku)
+    {
+        munLuku = 5;
+    }
+    
+    public static List<int> MuutaPaikkoja(List<int> lista, int a, int b)
+    {
+        List<int> uusiLista = new List<int>(lista);
+        int vaihdettava1 = uusiLista[a];
+        int vaihdettava2 = uusiLista[b];
+        // Tämä EI aiheuta sivuvaikutusta
+        // Ts. toimimme vain paikallisten muuttujien avulla.
+        // Pääohjelmassa ei tapahdu mitään "odottamatonta" muutosta. 
+        uusiLista[b] = vaihdettava1;
+        uusiLista[a] = vaihdettava2;
+        return uusiLista;
+    }
+    
+    /// <summary>
+    /// Aliohjelma, joka muuttaa parametrina saamaansa tietoa. 
+    /// </summary>
+    /// <param name="luvut">Taulukko lukuja</param>
+    /// <param name="a">Paikka a</param>
+    /// <param name="b">Paikka b</param>
+    public static void MuutaPaikkoja(int[] luvut, int a, int b)
+    {
+        int vaihdettava1 = luvut[a];
+        int vaihdettava2 = luvut[b];
+        // Tämä aiheuttaa sivuvaikutuksen:
+        luvut[b] = vaihdettava1;
+        luvut[a] = vaihdettava2;
+
+    }
+    
+    public static void A()
+    {
+        // luku++; // Tämä ei onnistu, koska luku on paikallinen Mainissa
+        munMuuttuja++;
     }
 }
--- Luento15/NakyvyysJypeli/NakyvyysJypeli.cs
+++ Luento15/NakyvyysJypeli/NakyvyysJypeli.cs
@@ -16,9 +16,101 @@ public class NakyvyysJypeli : PhysicsGame
 {
     public override void Begin()
     {
-        // Kirjoita ohjelmakoodisi tähän
+        SetWindowSize(1920, 1080);
+        CenterWindow();
+        Level.Size = new Vector(1920, 1080);
+        Level.CreateBorders();
+        List<GameObject> pallot = new List<GameObject>();
 
-        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        for (int i = 0; i < 50; i++)
+        {
+            LisaaPallo(pallot);
+        }
+       
+        for (int i = 0; i < 50; i++)
+        {
+            GameObject pallo = TeePallo(Level.BoundingRect);
+            Add(pallo);
+            pallot.Add(pallo);
+        }
+        
+        // Tämä tehtiin luennon jälkeen:
+        // Lisäsimme yllä palloja listaan sen takia, että voimme käsitellä
+        // palloja yhtenä kokonaisuutena. Luodaan tässä "pelaaja" (ts. 
+        // hiirellä liikuteltava neliö), jota lähellä olevia palloja
+        // värjäilemme valkoiseksi. 
+        GameObject pelaaja = new GameObject(30, 30, Shape.Rectangle);
+        pelaaja.Color = Color.Blue;
+        Add(pelaaja);
+
+        // Tehdään tässä hiiren liikkumisen kuuntelija, joka 
+        // 1. värjää ensin kaikki pallot valkoiseksi
+        // 2. ottaa talteen pelaajan sijainnin
+        // 3. etsii ja värjää punaiseksi kaikki ne pallot jotka ovat <200 etäisyyden päässä pelaajasta.
+        Mouse.ListenMovement(0.1, delegate()
+        {
+            foreach (GameObject pallo in pallot)
+            {
+                pallo.Color = Color.White;
+            }
+            pelaaja.Position = Mouse.PositionOnWorld;
+            // Jätetään vapaaehtoiseksi kotitehtäväksi, kuinka tämä
+            // tehtäisiin sivuvaikutuksettomaksi funktioksi.
+            // Ts. alkuperäistä pallot-taulukkoa ei muuteta
+            // vaan palautetaan täysin uusi taulukko, joka
+            // sijoitetaan pallot-muuttujaan
+            VarjaaLaheiset(pallot, pelaaja.Position, 200);
+        }, null);
+
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+    /// <summary>
+    /// Tekee yhden pallon satunnaiselle alueelle.
+    /// Tehty pallo palautetaan funktiosta. 
+    /// </summary>
+    /// <param name="alue">Mille alueelle pallo tehdään</param>
+    /// <returns>Pallo</returns>
+    static GameObject TeePallo(BoundingRectangle alue)
+    {
+        GameObject pallo = new GameObject(30, 30, Shape.Circle);
+        pallo.Position = RandomGen.NextVector(alue);
+        // pallo.Color = Color.Red;
+        return pallo;
+    }
+    
+    /// <summary>
+    /// TÄMÄ TEHTIIN LUENNON JÄLKEEN.
+    /// Värjää alle maxEtaisyyden päässä oelvat pallot punaiseksi.
+    /// Tämä aliohjelma on sivuvaikutuksellinen, koska se muuttaa
+    /// pallot-listaa. Se myöskin muuttaa pallon väriä, joka on
+    /// myöskin sivuvaikutus (vaikutuksen seuraukset näkyvät  käyttäjän ruudulla).
+    /// </summary>
+    /// <param name="pallot">Pallot</param>
+    /// <param name="piste">Piste</param>
+    /// <param name="maxEtaisyys">Max etäisyys jonka alle olevat pallot värjätään</param>
+    public void VarjaaLaheiset(List<GameObject> pallot, Vector piste, double maxEtaisyys)
+    {
+        foreach (GameObject pallo in pallot)
+        {
+            if (Vector.Distance(pallo.Position, piste) < maxEtaisyys)
+            {
+                pallo.Color = Color.Red;
+            }
+        }
+    }
+
+    
+    /// <summary>
+    /// Lisää pallo näytölle ja pallot-listaan.
+    /// </summary>
+    /// <param name="pallot">Pallot-lista</param>
+    void LisaaPallo(List<GameObject> pallot)
+    {
+        GameObject pallo = new GameObject(30, 30, Shape.Circle);
+        pallo.Position = RandomGen.NextVector(Level.BoundingRect);
+        pallo.Color = Color.White;
+        Add(pallo); // 1. sivuvaikutus
+        pallot.Add(pallo); // 2. sivuvaikutus
     }
 }
