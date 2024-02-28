a3c77d552c14d9ede66489b0cc794fd786931527
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2024-02-28 09:43:18 +0200

    Luento 16 alku

---
+++ Luento16/Rekursio/Rekursio.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// @author Omanimi
+/// @version 28.02.2024
+/// <summary>
+/// 
+/// </summary>
+public class Rekursio
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void Main()
+    {
+    }
+}
